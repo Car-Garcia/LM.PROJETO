b49398c7af19332beb74384355ece4cccb16d44b
Car-Garcia <carloseduardomng@gmail.com>
2023-05-23 13:42:37 -0300

    MEUS PROJETOS

---
+++ fake-news/script.js
@@ -0,0 +1,87 @@
+let box1 = document.querySelector('.box-1')
+let box2 = document.querySelector('.box-2')
+let box3 = document.querySelector('.box-3')
+let box4 = document.querySelector('.box-4')
+let box5 = document.querySelector('.box-5')
+let box6 = document.querySelector('.box-6')
+
+box1.onclick = () => {
+    //alert("clicou!!");
+    
+    if(box1.classList.contains('box-1')){
+    //alert("modo escuro inativo !!");
+    box1.classList.replace('box-1', 'box-01');
+    }else{
+        if(box1.classList.contains('box-01')){
+            //alert("modo escuro inativo !!");
+            box1.classList.replace('box-01', 'box-1');
+    }
+}
+}
+box2.onclick = () => {
+    //alert("clicou!!");
+    
+    if(box2.classList.contains('box-2')){
+    //alert("modo escuro inativo !!");
+    box2.classList.replace('box-2', 'box-02');
+    }else{
+        if(box2.classList.contains('box-02')){
+            //alert("modo escuro inativo !!");
+            box2.classList.replace('box-02', 'box-2');
+    }
+}
+}
+box3.onclick = () => {
+    //alert("clicou!!");
+    
+    if(box3.classList.contains('box-3')){
+    //alert("modo escuro inativo !!");
+    box3.classList.replace('box-3', 'box-03');
+    }else{
+        if(box3.classList.contains('box-03')){
+            //alert("modo escuro inativo !!");
+            box3.classList.replace('box-03', 'box-3');
+    }
+}
+}
+box4.onclick = () => {
+    //alert("clicou!!");
+    
+    if(box4.classList.contains('box-4')){
+    //alert("modo escuro inativo !!");
+    box4.classList.replace('box-4', 'box-04');
+    }else{
+        if(box4.classList.contains('box-04')){
+            //alert("modo escuro inativo !!");
+            box4.classList.replace('box-04', 'box-4');
+    }
+}
+}
+box5.onclick = () => {
+    //alert("clicou!!");
+    
+    if(box5.classList.contains('box-5')){
+    //alert("modo escuro inativo !!");
+    box5.classList.replace('box-5', 'box-05');
+    }else{
+        if(box5.classList.contains('box-05')){
+            //alert("modo escuro inativo !!");
+            box5.classList.replace('box-05', 'box-5');
+    }
+}
+}
+box6.onclick = () => {
+    //alert("clicou!!");
+    
+    if(box6.classList.contains('box-6')){
+    //alert("modo escuro inativo !!");
+    box6.classList.replace('box-6', 'box-06');
+    }else{
+        if(box6.classList.contains('box-06')){
+            //alert("modo escuro inativo !!");
+            box6.classList.replace('box-06', 'box-6');
+    }
+}
+}
+
+
